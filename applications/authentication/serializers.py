b6d5d28f2f50fd813c05ead89e65a78622fdb068
bhdrkcr <bahadir2kacar@gmail.com>
2021-04-30 23:13:06 +0300

    validate password in serializer

--- applications/authentication/serializers.py
+++ applications/authentication/serializers.py
@@ -1,3 +1,10 @@
+# Standard Library
+import sys
+
+# Django
+import django.contrib.auth.password_validation as validators
+from django.core import exceptions
+
 # Third Party
 from rest_framework import serializers
 
@@ -9,7 +16,35 @@ from .models import Registration, User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email"]
+        fields = ["first_name", "last_name", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Registration.objects.create(user=user)
+        return user
+
+    def validate(self, data):
+        # here data has all the fields which have validated values
+        # so we can create a User instance out of it
+        user = User(**data)
+
+        # get the password from the data
+        password = data.get("password")
+
+        errors = dict()
+        try:
+            # validate the password and catch the exception
+            validators.validate_password(password=password, user=User)
+
+        # the exception raised here is different than serializers.ValidationError
+        except exceptions.ValidationError as e:
+            errors["password"] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super(UserSerializer, self).validate(data)
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
@@ -18,4 +53,5 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Registration
+        lookup_field = "verification_key"
         exclude = []
