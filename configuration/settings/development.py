c9d21a62ad0c0f9e66c25765bc8c8055c57afb4c
bhdrkcr <bahadir2kacar@gmail.com>
2021-04-30 23:14:45 +0300

    console email backend on development

--- configuration/settings/development.py
+++ configuration/settings/development.py
@@ -1,5 +1,8 @@
 # Local Folder
 from .base import *
 
+# write emails to console, use this only in development
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
