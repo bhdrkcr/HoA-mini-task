916dbcd1319578aed4efcb02ca955eb1adfbea67
bhdrkcr <bahadir2kacar@gmail.com>
2021-04-30 23:12:53 +0300

    import signals

--- applications/authentication/apps.py
+++ applications/authentication/apps.py
@@ -1,7 +1,13 @@
 # Django
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "authentication"
+    verbose_name = _("authentication")
+
+    def ready(self):
+        # Third Party
+        import authentication.signals  # noqa
