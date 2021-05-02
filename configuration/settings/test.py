985a7290a67d94345de4f986aa2e27cbd29b0bcd
bhdrkcr <bahadir2kacar@gmail.com>
2021-05-02 12:23:32 +0300

    test settings

--- configuration/settings/test.py
+++ configuration/settings/test.py
@@ -11,3 +11,8 @@ DATABASES = {
         "NAME": "test_db",
     }
 }
+
+# testing recaptcha keys, use your own keys in production
+SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+RECAPTCHA_PUBLIC_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+RECAPTCHA_PRIVATE_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
