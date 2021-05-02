e63c4cd6fbceda4559a45c681a797e42fcb78cd7
bhdrkcr <bahadir2kacar@gmail.com>
2021-05-03 02:37:56 +0300

    form tests

--- applications/authentication/forms.py
+++ applications/authentication/forms.py
@@ -1,6 +1,7 @@
 # Django
 from django.conf import settings
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 # Third Party
@@ -29,7 +30,7 @@ class CustomLoginForm(AuthenticationForm):
     def confirm_login_allowed(self, user):
         super().confirm_login_allowed(user)
         if not user.is_staff:
-            raise self.ValidationError(
+            raise ValidationError(
                 self.error_messages["invalid_login"],
                 code="invalid_login",
                 params={"username": self.username_field.verbose_name},
--- applications/authentication/tests/test_forms/test_custom_login_form.py
+++ applications/authentication/tests/test_forms/test_custom_login_form.py
@@ -1,39 +1,60 @@
-# # Standard Library
-# from unittest.mock import MagicMock, PropertyMock, patch
+# Standard Library
+from unittest.mock import MagicMock, PropertyMock, patch
 
-# # Django
-# from django.contrib.auth.models import User
-# from django.test import TestCase  # Django
-# from django.urls import reverse
+# Django
+from django.test import TestCase  # Django
+from django.urls import reverse
+
+# Third Party
+from authentication.forms import CustomLoginForm
+from authentication.models import User
+from captcha.client import RecaptchaResponse
 
 
-# class TestCustomLoginForm(TestCase):
-#     def setUp(self):
-#         self.credentials = {"username": "testuser", "password": "ABC123secret"}
-#         self.login_url = "/admin/login"
-#         User.objects.create_user(**self.credentials)
+class TestCustomLoginForm(TestCase):
+    def setUp(self):
+        self.password = "Test123++1"
+        self.user = User.objects.create_superuser(
+            email="user@mail.com", password=self.password
+        )
 
-#     def test_login(self):
-#         # send login data
-#         response = self.client.post("/login/", self.credentials, follow=True)
-#         # should be logged in now
-#         self.assertTrue(response.context["user"].is_active)
-#         read_mock = MagicMock()
-#         read_mock.read.return_value = (
-#             b'{"success": true, "challenge_ts":'
-#             b'"2019-01-11T13:57:23Z", "hostname": "testkey.google.com"}'
-#         )
-#         mocked_response.return_value = read_mock
-#         uuid_hex = uuid.uuid4().hex
-#         response = client.submit(
-#             uuid_hex,
-#             "somekey",
-#             "0.0.0.0",
-#         )
+    @patch("captcha.fields.client.submit")
+    def test_login_with_captcha_passed(self, mocked_submit):
+        mocked_submit.return_value = RecaptchaResponse(is_valid=True)
+        # send login data to form directly
+        form = CustomLoginForm(
+            data={
+                "username": self.user.email,
+                "password": self.password,
+                "g-recaptcha-response": "PASSED",
+            }
+        )
+        self.assertTrue(form.is_valid())
 
-#         # Quick way to test method call without needing to worry about Python 2
-#         # dicts not being ordered by default.
-#         self.assertIn("secret=somekey", mocked_response.call_args.__str__())
-#         self.assertIn("response=%s" % uuid_hex, mocked_response.call_args.__str__())
-#         self.assertIn("remoteip=0.0.0.0", mocked_response.call_args.__str__())
-#         self.assertTrue(response.is_valid)
+    @patch("captcha.fields.client.submit")
+    def test_login_without_captcha_passed(self, mocked_submit):
+        mocked_submit.return_value = RecaptchaResponse(is_valid=False)
+        # send login data to form directly
+        form = CustomLoginForm(
+            data={
+                "username": self.user.email,
+                "password": self.password,
+                "g-recaptcha-response": "FAIL",
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    @patch("captcha.fields.client.submit")
+    def test_login_without_staff_user(self, mocked_submit):
+        mocked_submit.return_value = RecaptchaResponse(is_valid=True)
+        # send login data to form directly
+        self.user.is_staff = False
+        self.user.save()
+        form = CustomLoginForm(
+            data={
+                "username": self.user.email,
+                "password": self.password,
+                "g-recaptcha-response": "PASSED",
+            }
+        )
+        self.assertFalse(form.is_valid())
