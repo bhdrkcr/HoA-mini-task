95d815b28b2c2efe18920594db891ae74e19b1a4
bhdrkcr <bahadir2kacar@gmail.com>
2021-05-02 12:21:01 +0300

    create registration every time a user is created

--- applications/authentication/serializers.py
+++ applications/authentication/serializers.py
@@ -21,7 +21,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        Registration.objects.create(user=user)
         return user
 
     def validate(self, data):
