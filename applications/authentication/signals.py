505fb9297282a2b4ea1554ec8866f1d69bc0e52f
bhdrkcr <bahadir2kacar@gmail.com>
2021-05-02 12:20:44 +0300

    create token only if user is verified

--- applications/authentication/signals.py
+++ applications/authentication/signals.py
@@ -13,7 +13,9 @@ from .models import Registration
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
-        Token.objects.create(user=instance)
+        Registration.objects.create(user=instance)
+    if instance.is_verified:
+        Token.objects.get_or_create(user=instance)
 
 
 @receiver(post_save, sender=Registration)
